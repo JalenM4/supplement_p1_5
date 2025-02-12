22e29c4ed86b42c2c993bc6895e67116669852f4
Jalen Mason <jalen4@Jalens-MacBook-Air.local>
2025-02-12 18:40:45 -0500

    initial commit

---
+++ supplement_p1_5/Class1.cs
@@ -0,0 +1,12 @@
+﻿namespace supplement_p1_5;
+
+public class Class1
+{
+    public abstract class Shape3D
+    {
+        public abstract double GetVolume();
+        public abstract double GetSurfaceArea();
+        public abstract string Dump();
+    }
+
+}
