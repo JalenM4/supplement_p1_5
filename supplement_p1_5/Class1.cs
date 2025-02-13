a77cf229828a27a7ba165fe78a6c79d8f7182287
Jalen Mason <jalen4@Jalens-MacBook-Air.local>
2025-02-13 16:44:54 -0500

    implementation: added cylinder class

--- supplement_p1_5/Class1.cs
+++ supplement_p1_5/Class1.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
 
 namespace supplement_p1_5;
 
@@ -71,7 +72,67 @@ public class Class1
                 throw new ArgumentException("Radius must be greater than 0.");
             }
         }
-        public override do
+        public override double GetVolume()
+        {
+            return (4.0 / 3.0) * Math.PI * Math.Pow(Radius, 3);
+        }
+
+        public override double GetSurfaceArea()
+        {
+            return 4 * Math.PI * Math.Pow(Radius, 2);
+        }
+        public override string Dump()
+        {
+            return $"Sphere: Radius={Radius}, SurfaceArea={GetSurfaceArea()},Volume={GetVolume()}";
+        }
+    }
+    public class Cylinder : Shape3D
+    {
+        private double radius;
+        private double height;
+
+        public double Radius 
+        {
+            get => radius;
+            set
+            {
+                Validate(value, Height);
+                radius = value;
+            }
+        }
+        public double Height
+        {
+            get => height;
+            set
+            {
+                Validate(Radius, value);
+                height = value;
+            }
+        }
+        public Cylinder(double radius, double height)
+        {
+            Radius = radius;
+            Height = height;
+        }
+        private void Validate(double radius, double height)
+        {
+            if (radius <= 0 || height <= 0)
+            {
+                throw new ArgumentException("Radius and height must be greater than 0");
+            }
+        }
+        public override double GetVolume()
+        {
+            return Math.PI * Math.Pow(radius, 2) * Height;
+        }
+        public override double GetSurfaceArea()
+        {
+            return 2 * Math.PI * Radius * Radius * (Radius + Height);
+        }
+        public override string Dump()
+        {
+            return $"Cylinder : Radius={Radius}, Height={Height}, SurfaceArea={GetSurfaceArea()}, Volume={GetVolume()}";
+        }
     }
 
 }
