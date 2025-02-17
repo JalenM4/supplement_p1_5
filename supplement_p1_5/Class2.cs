f7e4e314b0212437eb494220b04de85fcca0b904
Jalen Mason <jalen4@Jalens-MacBook-Air.local>
2025-02-17 14:25:42 -0500

    implementation: implemented shapecontainer to add shapes at specific index

--- supplement_p1_5/Class2.cs
+++ supplement_p1_5/Class2.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Collections.Generic;
 
-namespace Shapes;
-
+namespace Shapes
+{
 public class Shape3D
 {
     public string Name { get ; set;}
@@ -15,4 +15,44 @@ public class Shape3D
     {
         return Name;
     }
+}
+
+public class ShapeContainer
+{
+   // list to store shapes
+    private List<Shape3D> shapes;
+
+    // initilizing constructor
+    public ShapeContainer()
+    {
+        shapes = new List<Shape3D>();
+    }
+
+    // method that adds shape
+    public void Create(Shape3D shape)
+    {
+        if (shape == null)
+            throw new ArgumentNullException(nameof(shape), "Shape cannot be null");
+
+        shapes.Add(shape);
+    }
+
+    // gets and retrieves the shape at specified index
+    public Shape3D Get(int index)
+    {
+        if (index < 0 || index >= shapes.Count)
+            throw new ArgumentOutOfRangeException(nameof(index), "Index out of range");
+
+        return shapes[index];
+    }
+
+    // deletes shape from specified index
+    public void Delete(int index)
+    {
+        if(index < 0 || index >= shapes.Count)
+            throw new ArgumentOutOfRangeException(nameof(index), "Index is out of range");
+
+        shapes.RemoveAt(index);
+    }
+}
 }
